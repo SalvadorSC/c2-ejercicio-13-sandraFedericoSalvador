6d4043f42298372f87645cd2255461d7cb5f7ed0
Salvador Sánchez <sanchezcampossalvador@gmail.com>
2021-06-01 22:58:32 +0200

    totalbase hecho por sandra y vencimiento

--- js/index.js
+++ js/index.js
@@ -2,8 +2,9 @@ import { facturas } from "../datos/facturas.js";
 
 const tablaFacturas = document.querySelector(".tablaFacturas");
 console.log(tablaFacturas);
+let totalBaseMarronero = 0;
 
-for (const { numero, tipo } of facturas) {
+for (const { numero, tipo, fecha, abonada, vencimiento, base } of facturas) {
   if (tipo === "ingreso") {
     console.log(numero);
     const facturaElemento = document
@@ -16,7 +17,12 @@ for (const { numero, tipo } of facturas) {
     numFactura.textContent = numero;
 
     // Fecha Factura
+    const fechaFactura = facturaElemento.querySelector(".fechaFactura");
+    const dateFactura = new Date(fecha);
 
+    fechaFactura.textContent = `${dateFactura.getDate()}/${
+      dateFactura.getMonth() + 1
+    }/${dateFactura.getFullYear()}`;
     // Concepto Factura
 
     // Base
@@ -25,9 +31,36 @@ for (const { numero, tipo } of facturas) {
 
     // TOTAL
 
+    // TOTAL Base
+    const totalBase = document.querySelector(".total-base");
+    // const calculoTotalBase = facturas.reduce((acumulador, {base}) => base + acumulador,0);
+
+    totalBaseMarronero = base + totalBaseMarronero;
+    totalBase.textContent = `${totalBaseMarronero} €`;
+
     // Estado Factura
 
     // Vencimiento Factura
+    const vencimientoFactura = facturaElemento.querySelector(
+      ".vencimientoFactura"
+    );
+    const dateVencimiento = new Date(vencimiento);
+
+    if (abonada) {
+      vencimientoFactura.textContent = "-";
+      vencimientoFactura.classList.add("table-success");
+    } else if (!abonada && vencimiento - fecha < 0) {
+      vencimientoFactura.textContent = `${dateVencimiento.getDate()}/${
+        dateVencimiento.getMonth() + 1
+      }/${dateVencimiento.getFullYear()} (dentro de X días)`;
+      vencimientoFactura.classList.add("table-success");
+    } else {
+      vencimientoFactura.textContent = `${dateVencimiento.getDate()}/${
+        dateVencimiento.getMonth() + 1
+      }/${dateVencimiento.getFullYear()} (hace X días)`;
+      vencimientoFactura.classList.add("table-danger");
+    }
+    //
 
     tablaFacturas.append(facturaElemento);
   }
